448d97aac8000ee019d4b509c04532dddc784d26
DhirajSurvase <dms25121997@gmail.com>
2018-03-19 09:55:32 +0000

    add compile script

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#DhirajMS
+#Compile script for kernel
+
+
+#clean out dir
+#rm -rf out
+mkdir -p out
+
+
+#ARCH
+export ARCH=arm
+
+
+#host and user
+#export KBUILD_BUILD_USER="DhirajMS"
+#export KBUILD_BUILD_HOST="Dracarys"
+
+
+#get toolchain gcc-4.8
+git clone https://github.com/DhirajSurvase/GCC_4.8 gcc4.8
+export CROSS_COMPILE=/home/younilban/kernel/a6000/gcc4.8/bin/arm-eabi-
+
+#tc>arm-linaro-7.x
+#git clone --recursive https://github.com/nathanchance/gcc-prebuilts -b arm-linaro-7.x arm-linaro-7.x
+#export CROSS_COMPILE=/home/younilban/kernel/a6000/arm-linaro-7.x/bin/arm-linaro-linux-androideabi-
+
+
+#get toolchain gcc-4.9
+#git clone https://github.com/DhirajSurvase/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc4.9
+#export CROSS_COMPILE=/home/ubuntu/kcp/gcc4.9/bin/arm-linux-androideabi-
+
+
+#Defconfig for Moto C plus
+#make -C $PWD O=$PWD/out ARCH=arm A158_defconfig
+make -C $PWD O=$PWD/out ARCH=arm lineageos_a6000_defconfig
+
+
+#CompileNow
+make -j64 -C $PWD O=$PWD/out ARCH=arm
